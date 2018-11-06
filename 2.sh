2a998aae2eacebc50a2865b487a6abbd3d1c791e
lowkbyza <43971089+lowkbyza@users.noreply.github.com>
2018-11-06 07:35:22 +0700

    Delete 2.sh

--- 2.sh
+++
@@ -1,7 +0,0 @@
-cd /home/vps/public_html 
-wget https://github.com/lowkbyza/al/raw/master/2.zip 
-mv install.zip LTEOCS.zip 
-unzip LTEOCS.zip 
-rm -f LTEOCS.zip 
-chown -R www-data:www-data /home/vps/public_html 
-chmod -R g+rw /home/vps/public_html
